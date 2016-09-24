426a39d61ec59276f3e70682825b114fea5ce404
ScottMillerWesCoas <scott.t.miller1@gmail.com>
2016-09-24 10:25:10 -0700

    modularized scoreBoard component

--- public/assets/components.js
+++ public/assets/components.js
@@ -187,27 +187,11 @@ class Game extends React.Component {
     }
 }
 
- class ScoreBoard extends React.Component{
-    render(){ 
-        return(
-        <div className='scoreBoard'>
-        <span className='score1' style={{'color':'blue','float': 'left', 'fontWeight': 'bold'}}>Score: {this.props.state.score}</span>
-          <ul>
-              {this.props.state.submitArr.map(function(elem, i){
-                return <li className='word2' style={{'color': 'red', 'fontWeight': 'bold'}} key={i}>{elem}</li>;
-              })
-              }
-          </ul>
-        </div>
-        )
-    }
-
- }
 
 
-// jQuery(function() {
+
   ReactDOM.render(
         <Game />,
     document.getElementById('game-box')
   );
-// })
+
